88a106d82d7ae769c1210a5cbf7503eedcc92c1f
hehecoi222 <hehecoi@outlook.com>
2021-08-31 18:59:12 +0700

    Make book.go fancier

--- book/book.go
+++ book/book.go
@@ -45,9 +45,9 @@ func check(hor int, ver int, input [][]int) bool {
 
 func main() {
 	var input = [][]int{
-		[]int{3, 3, 2},
-		[]int{4, 1, 3},
-		[]int{3, 2, 0},
+		{3, 3, 2},
+		{4, 1, 3},
+		{3, 2, 0},
 	}
 	possible := math.Pow(float64(n), 2)
 	for i := 1; i < n; i++ {
