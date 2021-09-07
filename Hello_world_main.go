742cb0fbcfc3523d6480ec4b0893e2699367c1ad
hehecoi222 <hehecoi@outlook.com>
2021-09-07 16:43:28 +0700

    Add printing NumCPU, NumGoroutine, GOOS, GOARCH

--- Hello_world_main.go
+++ Hello_world_main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func main(){
+	fmt.Println(runtime.GOOS)
+	fmt.Println(runtime.GOARCH)
+	fmt.Println(runtime.NumCPU())
+	fmt.Println(runtime.NumGoroutine())
 	fmt.Println("Hello,  世界")
 }
