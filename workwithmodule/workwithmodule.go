f54552e637878acd2ba8fcbf2584330911385a56
hehecoi222 <hehecoi@outlook.com>
2021-09-17 23:12:57 +0700

    Add documentation

--- workwithmodule/workwithmodule.go
+++ workwithmodule/workwithmodule.go
@@ -1,9 +1,11 @@
+// A test with module in Go
 package workwithmodule
 
 import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// CryptPass is used to create an bcrypt hashed password
 func CryptPass(Ps string) []byte {
 	Pscrypt, err := bcrypt.GenerateFromPassword([]byte(Ps), bcrypt.MinCost)
 	if err != nil {
